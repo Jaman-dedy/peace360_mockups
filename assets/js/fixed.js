16a747094c920f09d1aba9c66281adf59f3d2eef
Jaman Dedy <jeandedieuam@gmail.com>
2019-11-28 14:32:53 +0200

    posted comment

--- assets/js/fixed.js
+++ assets/js/fixed.js
@@ -1,4 +1,4 @@
 $(window).scroll(function() {
   $("#socio-share").css("top", Math.max(200, 370 - $(this).scrollTop()));
-//   $("#socio-share").css("display", Math.max(0, 200 - $(this).scrollTop()));
+  //   $("#socio-share").css("display", Math.max(0, 200 - $(this).scrollTop()));
 });
